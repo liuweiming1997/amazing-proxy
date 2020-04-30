d070e886e1142f1fa69f8295f43aa71552761589
weimingliu <vimiming@gmail.com>
2020-05-01 00:02:26 +0800

    format info

--- src/main.go
+++ src/main.go
@@ -2,18 +2,25 @@ package main
 
 import (
 	// "encoding/binary"
+	"fmt"
 	"net"
 	"os"
 
 	log "github.com/sirupsen/logrus"
 )
 
+const (
+	ServerAddress = "0.0.0.0"
+	ServerPort = "9090"
+)
+
 func init() {
-	log.Info("Running amazing-server with pid = ", os.Getpid())
+	log.Info("Running amazing-proxy with pid = ", os.Getpid())
+	log.Info(fmt.Sprintf("Process listen host = %s:%s", ServerAddress, ServerPort))
 }
 
 func main() {
-	addr, err := net.ResolveUDPAddr("udp", "localhost:9090")
+	addr, err := net.ResolveUDPAddr("udp", fmt.Sprintf("%s:%s", ServerAddress, ServerPort))
 	if err != nil {
 		log.Error("Can not resolve host, exit...", err)
 		os.Exit(1)
